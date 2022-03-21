7bb7b26c84d5632a1715d50c0bcc80781e00042f
elinb29 <elinbog@yandex.ru>
2022-03-21 10:38:48 +0300

    Initial commit

---
+++ lib/screens/explore_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ExploreScreen extends StatelessWidget {
+  @override
+Widget build(BuildContext context) {
+    return Center(child: Text('My screen'));
+  }
+
+}
---
+++ lib/screens/recipes_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class RecipesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('My screen'));
+  }
+
+}
